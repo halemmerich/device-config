425b6465845d80e79c04bab16034c900180f2e20
Martin Boonk <martin@boonk.info>
2021-10-23 17:25:38 +0200

    Create fake certifiates with SAN

--- files/fake-ca/createCA.sh
+++ files/fake-ca/createCA.sh
@@ -2,3 +2,5 @@
 
 openssl genrsa -out ca.key 4096
 openssl req -new -x509 -days 3650 -key ca.key -out ca.pem -subj "/C=FK/ST=FAKE/L=FAKE/O=FAKE/OU=CA/CN=CA"
+
+openssl x509 -inform PEM -in ca.pem -out ca.crt
